0e0337b7b6c2643721609638178704528100d8b0
Max <alfmaxhdz@gmail.com>
2018-05-24 10:32:08 -0700

    fix

---
+++ controllers/storeData.js
@@ -0,0 +1,73 @@
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://hernandez:i84k8@ds243285.mlab.com:43285/heroku_9hbcfksr';
+var express = require('express');
+var router = express.Router();
+
+//to process data sent in on request need body-parser module
+var bodyParser = require('body-parser');
+var path = require('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencode
+
+module.exports.storeData = function (req, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+        if(err) throw err;
+
+
+        //get handle to the databse
+        var theDatabase = client.db('heroku_9hbcfksr');
+
+        //var customerData = req.body.customer;
+        //var orderData  = req.body.order;
+
+        var customerID = Math.floor((Math.random() * 1000000000000) + 1);
+        var billingID = Math.floor((Math.random() * 1000000000000) + 1);
+        var shippingID = Math.floor((Math.random() * 1000000000000) + 1);
+
+        var CUSTOMERS = theDatabase.collection('customer');
+        var ORDERS = theDatabase.collection('shipping');
+        var BILLING = theDatabase.collection('billing')
+        /*
+
+        var customerdata = {
+            _id: customerID,
+            first_name: ['fname'],
+            last_name: shipment_info['lname'],
+            street: shipment_info['add1'],
+            city: shipment_info['city'],
+            state: shipment_info['state'],
+        };
+
+        var orderdata = {
+            customer_id: customerID,
+            billing_id: billingID,
+            shipping_id: shippingID,
+            product_vector: orderData,
+            order_total: req.body.subtotal
+        }
+        */
+        var billingdata = {
+            _id: billingID,
+        };
+        /*
+        var shippingdata = {
+            _id: shippingID,
+            customer_id: customerID,
+            shipping_street: req.body.customer.streetAddress,
+            shipping_city: req.body.customer.city,
+            shipping_state: req.body.customer.state
+        }
+    */
+        /*
+        CUSTOMERS.insertOne(customerdata, function (err, result) {
+            if (err) throw err;
+        })
+        */
+        BILLING.insertOne(billingdata, function (err, result) {
+            if (err) throw err;
+        });
+        response.render('result', {});
+    };
+};
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var router = express.Router();
 //LOAD the various controllers
 //var controllerMain = require('../controllers/main');   //this will load the main controller file
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+var controllerMongoCollectionStore = require('../controllers/storeData');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,7 +27,7 @@ router.post('/storeData', function(req, res, next) {
 
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
-router.post("/storeData", controllerMongoCollection.storeData);
+router.post("/storeData", controllerMongoCollectionStore.storeData);
 
 
 
