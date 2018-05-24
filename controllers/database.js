6ae3b44fa00402872b0ed312d2485614e8e47dcc
Max <alfmaxhdz@gmail.com>
2018-05-24 01:23:02 -0700

    test

--- controllers/database.js
+++ controllers/database.js
@@ -13,7 +13,7 @@ module.exports.getAllOrders =  function (request, response) {
 
 
         //get collection of routes
-        var Orders = theDatabase.collection('orders');
+        var Orders = theDatabase.collection('orders'); 
         //var Billing = theDatabase.collection('billing');
         //var Shipping = theDatabase.collection('shipping');
         //var Customer = theDatabase.collection('customer');
